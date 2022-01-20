4a612f88ae4775c78e1decfdbd2733f39289934d
HoussamEddineSEHLAOUI <sehlaoui.houssameddine@gmail.com>
2022-01-20 01:14:54 +0100

    add Moyenne

--- src/Components/Table/Table.js
+++ src/Components/Table/Table.js
@@ -13,6 +13,78 @@ class Table extends Component {
     }
   }
 
+  //moyennne NCE
+  MoyenneNCE(outputs){
+    var moy = 0 ;
+    outputs.forEach(element =>{
+      moy=moy+element.NCE ;
+    })
+    return moy / outputs.length ;
+  }
+
+  //moyenne NCP
+  MoyenneNCP(outputs){
+    var moy = 0 ;
+    outputs.forEach(element =>{
+      moy=moy+element.NCP ;
+    })
+    return moy / outputs.length ;
+  }
+
+  //Moyenne TATmoy
+  MoyenneTATmoy(outputs){
+    var moy = 0 ;
+    outputs.forEach(element =>{
+      moy=moy+parseInt(element.TATmoy) ;
+    })
+    return moy / outputs.length ;
+  }
+
+  //moyenne TSmy
+  MoyenneTSmy(outputs){
+    var moy = 0 ;
+    outputs.forEach(element =>{
+      moy=moy+parseInt(element.TSmy) ;
+    })
+    return moy / outputs.length ;
+  }
+
+  // Moyenne tauxC1
+  MoyennetauxC1(outputs){
+    var moy = 0 ;
+    outputs.forEach(element =>{
+      moy=moy+element.tauxC1 ;
+    })
+    return moy / outputs.length ;
+  }
+
+  // Moyenne tauxC2
+  MoyennetauxC2(outputs){
+    var moy = 0 ;
+    outputs.forEach(element =>{
+      moy=moy+element.tauxC2 ;
+    })
+    return moy / outputs.length ;
+  }
+
+  // Moyenne tauxC3
+  MoyennetauxC3(outputs){
+    var moy = 0 ;
+    outputs.forEach(element =>{
+      moy=moy+element.tauxC3 ;
+    })
+    return moy / outputs.length ;
+  }
+
+  // Moyenne DFS
+    MoyenneDFS(outputs){
+      var moy = 0 ;
+      outputs.forEach(element =>{
+        moy=moy+element.DFS ;
+      })
+      return moy / outputs.length ;
+    }
+
   render(){
     const outputs = this.props.outputs ;
     const page = this.props.page ;
@@ -39,7 +111,8 @@ class Table extends Component {
                     </thead>
                     <tbody>
                     {outputs.map(output=>{
-                          return(<tr>
+                          return(
+                          <tr>
                               <td >{output.index+1}</td>
                               <td >{output.NCE}</td>
                               <td >{output.NCP}</td>
@@ -52,8 +125,25 @@ class Table extends Component {
                               :<></>
                               }
                               <th>{output.DFS}</th>
-                          </tr>);
-                          })}
+                          </tr>
+                          );
+                      })}
+
+                            <tr>
+                              <td >Moyenne</td>
+                              <td >{this.MoyenneNCE(outputs)}</td>
+                              <td >{this.MoyenneNCP(outputs)}</td>
+                              <td >{this.MoyenneTATmoy(outputs)}</td>
+                              <td >{this.MoyenneTSmy(outputs)}</td>
+                              <td >{this.MoyennetauxC1(outputs).toFixed(2)}</td>
+                              <td >{this.MoyennetauxC2(outputs).toFixed(2)}</td>
+                              {page !== 1
+                              ? <td >{this.MoyennetauxC3(outputs).toFixed(2)}</td>
+                              :<></>
+                              }
+                              <th>{this.MoyenneDFS(outputs)}</th>
+                          </tr>
+
                     </tbody>
                 </table>
             </div>
